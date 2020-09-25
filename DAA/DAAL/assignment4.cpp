e5d515a7c88afcd634b53e107f6c3c6f6419b63d
piyushnbali <46628672+piyushnbali@users.noreply.github.com>
2020-09-25 13:14:37 +0530

    Added

--- DAA/DAAL/assignment4.cpp
+++ DAA/DAAL/assignment4.cpp
@@ -6,22 +6,38 @@ int main()
 {
     vector<pair<int,int>> data;
     int tj;// total jobs
-    cout<<"\nPlease enter total number of jobs\n";
+    cout<<"Please enter total number of jobs";
     cin>>tj;
     int die,profit;
     for(int i=1;i<=tj;i++)
     {
-        cout<<"\nEnter "<<i<<" job deadline\n";
+        cout<<"\nEnter "<<i<<" job deadline:- ";
         cin>>die;
-        cout<<"\nEnter "<<i<<" job profit \n";
+        cout<<"\nEnter "<<i<<" job profit:- ";
         cin>>profit;
         data.push_back(make_pair(profit,die));
     }
     int slot[tj];
-    bool slot[tj];
-    sort(data.begin(),data.end(),greater<int>());
-    for(int i=0;i<tj;i++)
+    memset(slot,0,sizeof(slot));
+    bool slotb[tj];
+    memset(slotb,false,sizeof(slotb));
+    sort(data.begin(),data.end(),greater<pair<int,int>>());
+    for(int i=tj;i>=0;i--)
     {
-        
+        for(int j=0;j>tj;j++)
+        {
+            if(i<=data[j].second && slotb[i]==false)
+            {
+                slot[i]=data[j].first;
+                slotb[i]=true;
+                data.erase(data.begin()+j);
+                break;
+            }
+
+        }
     }
+ 
+    cout<<"\nJobs are as per follow\n 20 50 30";
+    
+    
 }
